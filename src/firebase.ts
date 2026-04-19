e207522595536d7e2841da7e6be3762fa3b95b30
ifaizul500-rgb <ifaizul500@gmail.com>
2026-04-19 19:15:07 +0600

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,11 @@ import { getAuth } from 'firebase/auth';
 import { initializeFirestore } from 'firebase/firestore';
 import firebaseConfig from '../firebase-applet-config.json';
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Services with enhanced connectivity settings
 export const auth = getAuth(app);
 export const db = initializeFirestore(app, {
-  experimentalForceLongPolling: true,
+  experimentalAutoDetectLongPolling: true,
 }, firebaseConfig.firestoreDatabaseId);
 
 export default app;
